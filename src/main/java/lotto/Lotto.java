31c643f234d5df4ff92efb006853bd830e16491f
Kim Byeong-Min <qudals7214@gmail.com>
2022-11-15 22:06:05 +0900

    커밋

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,8 +1,13 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
+
+    //내가 구매한 로또 용지
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -11,7 +16,11 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
+        Set<Integer> duplicationNumber = new HashSet<>(numbers);
+        numbers = new ArrayList<>(duplicationNumber);
+
         if (numbers.size() != 6) {
+            System.err.println("[ERROR] 숫자 확인");
             throw new IllegalArgumentException();
         }
     }
